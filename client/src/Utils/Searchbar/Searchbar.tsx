47989a21e1bea8b17edc7038684ca9123ed9e74a
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-05-01 17:47:54 +0530

    client(functionality/searchbar): add search icon hover effect

--- client/src/Utils/Searchbar/Searchbar.tsx
+++ client/src/Utils/Searchbar/Searchbar.tsx
@@ -1,10 +1,14 @@
 // ICONS
 import { IoSearchSharp } from 'react-icons/io5';
+import { MdKeyboardCommandKey } from 'react-icons/md';
 
 const Searchbar = () => {
   return (
-    <div className="HOVER-BUTTON">
+    <div className="FLEX-CENTER HOVER-LINK TEXT hover:bg-black">
       <IoSearchSharp size={20} />
+      <div className="FLEX-CENTER">
+        <MdKeyboardCommandKey /> + K
+      </div>
     </div>
   );
 };
