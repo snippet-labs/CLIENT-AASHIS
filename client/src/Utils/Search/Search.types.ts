522a402bebd0f59e9a4c9f6534bc6ff9d044b3cf
Hemangshu-Dey <hemangshu1904@gmail.com>
2025-05-10 15:05:16 +0530

    client(fix/searchbar): mobile view

--- client/src/Utils/Search/Search.types.ts
+++ client/src/Utils/Search/Search.types.ts
@@ -1,19 +1,21 @@
 export interface SearchModalProps {
-    isOpen: boolean;
-    isMac: boolean;
-    onClose: () => void;
+  isOpen: boolean;
+  isMac: boolean;
+  windowSize: number;
+  onClose: () => void;
 }
 
 export interface SearchProps {
   isMac: boolean;
+  windowSize: number;
 }
 
 export interface RecentSearchesProps {
-    searches: string[];
-    onSelectSearch: (search: string) => void;
-    onClearSearches: () => void;
+  searches: string[];
+  onSelectSearch: (search: string) => void;
+  onClearSearches: () => void;
 }
 
 export type SearchBarProps = {
-    onSearch: (term: string) => void;
+  onSearch: (term: string) => void;
 };
