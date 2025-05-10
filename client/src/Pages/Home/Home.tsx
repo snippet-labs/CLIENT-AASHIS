b9c4fe0256b3e1fc27038b4fbc9df069930f2f9b
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-05-10 12:54:14 +0530

    client(page/home): add global style for background & colors; use hook; use utils

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+// ICONS
+import { VscSettings } from 'react-icons/vsc';
+
+// COMPONENTS
+import Button from '../../Utils/Button/Button';
+// HOOKS
+import useWindowSize from '../../Hooks/useWindowSize';
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  // HOOKS
+  const windowSize: number = useWindowSize();
+  return (
+    <div>
+      <span>
+        <Button
+          title={'SHOW CATEGORIES'}
+          windowSize={windowSize}
+          className="TEXT PADDING ROUNDED PRIMARY-GRAY"
+          icon={<VscSettings size={20} />}
+        />
+      </span>
+    </div>
+  );
 };
 
 export default Home;
